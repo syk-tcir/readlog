3a59c14097047e6949ef1bd108b6cbbf0255eb7a
syk-tcir <sykt.819@gmail.com>
2026-04-04 21:08:18 +0900

    app/urls.py修正

--- readlog/app/urls.py
+++ readlog/app/urls.py
@@ -2,15 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'), 
+    path('', views.top, name='top'),
+    path('home/', views.index, name='home'),
     path('test/', views.api_test, name='api_test'),
     path('books/register/', views.register_book, name='register_book'),
     path('books/register/detail/', views.book_register_detail, name='book_register_detail'),
-    path('books/check/', views.check_book_exists, name='check_book_exists'),
     path('books/check/', views.check_book_exists, name='check_book_exists'),
     path('books/', views.book_list, name='book_list'),
     path('books/<int:book_id>/', views.book_detail, name='book_detail'),
     path('books/<int:book_id>/edit/', views.book_edit, name='book_edit'),
     path('books/<int:book_id>/delete/', views.book_delete, name='book_delete'),
-    path('top/', views.top, name='top'),
 ]
