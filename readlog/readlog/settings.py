ef2a310cf2aaf91741d82a0d6cc413161217b576
syk-tcir <sykt.819@gmail.com>
2026-04-05 22:04:41 +0900

    ALLOWED_HOSTS修正

--- readlog/readlog/settings.py
+++ readlog/readlog/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'sayokotachiiri.pythonanywhere.com']
 
 
 # Application definition
